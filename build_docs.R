3d374449da8d8b25ae2d1442bbd26e00d18dbb53
Yiyan Shou <yiyanshou@gmail.com>
2026-05-02 23:52:57 -0400

    Set up for deployment on Posit Connect Cloud.

--- build_docs.R
+++ build_docs.R
@@ -3,5 +3,8 @@ shinylive::export("compare_LRC",
                   "docs",
                   template_params = list(title = "LRC Comparison"))
 
+# Generate manifest for Posit Connect
+rsconnect::writeManifest("compare_LRC")
+
 # Run app locally
 httpuv::runStaticServer("docs")
